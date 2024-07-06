a4c71136df27eecf52ca821b803b8dd9bb9e93e8
Daniel <daneilcipriani321@gmail.com>
2024-07-06 16:15:17 -0300

    Criada classe utilConstants

---
+++ src/main/java/frc/robot/util/UtilConstants.java
@@ -0,0 +1,12 @@
+package frc.robot.util;
+
+import edu.wpi.first.math.util.Units;
+
+public final class UtilConstants {
+
+    public static class VisionConstants{
+        public static final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(30);
+        public static final double CAMERA_HEIGHT_METERS = 0.20;
+    }
+    
+}
