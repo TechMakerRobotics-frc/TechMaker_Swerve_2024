d1e2dd58aa69cd2d7f470e34706bbee78218ddb9
Daniel <daneilcipriani321@gmail.com>
2024-07-06 23:01:04 -0300

    re-organizados e criada cosntante camera name

--- src/main/java/frc/robot/util/UtilConstants.java
+++ src/main/java/frc/robot/util/UtilConstants.java
@@ -4,9 +4,9 @@ import edu.wpi.first.math.util.Units;
 
 public final class UtilConstants {
 
-    public static class VisionConstants{
-        public static final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(30);
-        public static final double CAMERA_HEIGHT_METERS = 0.20;
-    }
-    
+  public static class VisionConstants {
+    public static final String CAMERA_NAME = "AprilTags";
+    public static final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(30);
+    public static final double CAMERA_HEIGHT_METERS = 0.20;
+  }
 }
