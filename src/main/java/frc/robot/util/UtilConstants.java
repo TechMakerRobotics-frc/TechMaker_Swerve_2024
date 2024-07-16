a58ad43d25fb43431eeb64d335decd2e01344bed
Daniel <daneilcipriani321@gmail.com>
2024-07-15 23:21:00 -0300

    Alterado nome da constante câmera adicionado "a" para identificar outras câmeras.

--- src/main/java/frc/robot/util/UtilConstants.java
+++ src/main/java/frc/robot/util/UtilConstants.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.util.Units;
 public final class UtilConstants {
 
   public static class VisionConstants {
-    public static final String CAMERA_NAME = "SuperCam";
+    public static final String CAMERA_A_NAME = "SuperCam";
     public static final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(30);
     public static final double CAMERA_HEIGHT_METERS = 0.20;
 
