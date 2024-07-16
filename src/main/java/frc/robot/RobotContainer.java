24f6bb7b4f8097c0acfd40bcfec8a9d99850bd2c
TechMaker <sesi.techmakerrobotics@gmail.com>
2024-07-16 16:13:11 -0300

    Alterado PhotonPose para adicionar pose do robô

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,19 +13,20 @@
 
 package frc.robot;
 
-import frc.robot.Constants.Mode;
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.Mode;
 import frc.robot.commands.AlignCommand;
 import frc.robot.commands.DriveCommands;
 import frc.robot.subsystems.drive.*;
 import frc.robot.subsystems.flywheel.*;
 import frc.robot.subsystems.intake.*;
 import frc.robot.util.PhotonVision.PhotonSim;
+import frc.robot.util.PhotonVision.PhotonPose;
 import frc.robot.util.TunningPID;
 import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
 
@@ -112,9 +113,10 @@ public class RobotContainer {
             .withTimeout(5.0));
     // Configure the button bindings
     configureButtonBindings();
-    if(Constants.currentMode == Mode.SIM){
+    if (Constants.currentMode == Mode.SIM) {
       new PhotonSim(drive);
     }
+    new PhotonPose();
   }
 
   /**
--- src/main/java/frc/robot/util/PhotonVision/PhotonPose.java
+++ src/main/java/frc/robot/util/PhotonVision/PhotonPose.java
@@ -4,40 +4,50 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.util.UtilConstants.VisionConstants;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class PhotonPose {
 
-  public Pose2d getRobotPose() {
-    PhotonCamera camera = PhotonTags.getCamera();
-    PhotonTrackedTarget bestTarget = camera.getLatestResult().getBestTarget();
+  Field2d field;
 
-    Transform2d cameraToTarget =
-        new Transform2d(
-            new Translation2d(
-                bestTarget.getBestCameraToTarget().getX(),
-                bestTarget.getBestCameraToTarget().getY()),
-            Rotation2d.fromDegrees(bestTarget.getYaw()));
+  public PhotonPose() {
+    PhotonPipelineResult p = PhotonTags.getLatestPipeline();
 
-    Pose2d fieldToTarget =
-        new Pose2d(
-            VisionConstants.TARGET_7_X_POSITION_METERS,
-            VisionConstants.TARGET_7_Y_POSITION_METERS,
-            Rotation2d.fromDegrees(VisionConstants.TARGET_7_YAW_DEGREES));
+    if (PhotonTags.hasTarget(p)) {
+      PhotonCamera camera = PhotonTags.getCamera();
+      PhotonTrackedTarget bestTarget = camera.getLatestResult().getBestTarget();
 
-    // Construct cameraToRobot
-    Transform2d cameraToRobot =
-        new Transform2d(
-            new Translation2d(
-                VisionConstants.CAMERA_POSITION_FROM_ROBOT_CENTER_X_METERS,
-                VisionConstants.CAMERA_POSITION_FROM_ROBOT_CENTER_Y_METERS),
-            Rotation2d.fromDegrees(0));
+      Transform2d cameraToTarget =
+          new Transform2d(
+              new Translation2d(
+                  bestTarget.getBestCameraToTarget().getX(),
+                  bestTarget.getBestCameraToTarget().getY()),
+              Rotation2d.fromDegrees(bestTarget.getYaw()));
 
-    Pose2d robotPose2d =
-        PhotonUtils.estimateFieldToRobot(cameraToTarget, fieldToTarget, cameraToRobot);
-    return robotPose2d;
+      Pose2d fieldToTarget =
+          new Pose2d(
+              VisionConstants.TARGET_7_X_POSITION_METERS,
+              VisionConstants.TARGET_7_Y_POSITION_METERS,
+              Rotation2d.fromDegrees(VisionConstants.TARGET_7_YAW_DEGREES));
+
+      // Construct cameraToRobot
+      Transform2d cameraToRobot =
+          new Transform2d(
+              new Translation2d(
+                  VisionConstants.CAMERA_POSITION_FROM_ROBOT_CENTER_X_METERS,
+                  VisionConstants.CAMERA_POSITION_FROM_ROBOT_CENTER_Y_METERS),
+              Rotation2d.fromDegrees(0));
+
+      Pose2d robotPose2d =
+          PhotonUtils.estimateFieldToRobot(cameraToTarget, fieldToTarget, cameraToRobot);
+      field.setRobotPose(robotPose2d);
+      SmartDashboard.putData("Field robot pose PhotonVision", field);
+    }
   }
 }
