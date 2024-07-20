7deca3679d7da5e0ad8836f05ffa2f37d1ee6a6e
23Daniel3 <daneilcipriani321@gmail.com>
2024-07-19 23:44:08 -0300

    Classe para obter altura da apriltag e da câmera

--- src/main/java/frc/robot/util/Height.java
+++ src/main/java/frc/robot/util/Height.java
@@ -1,11 +1,13 @@
 package frc.robot.util;
 
 import edu.wpi.first.math.util.Units;
+import frc.robot.util.UtilConstants.VisionConstants;
 
 /** Classe para obter a altura do alvo com base no ID do AprilTag. */
-public class IdTargetHeight {
+public class Height {
 
   private static double targetHeight;
+
 
   /**
    * Retorna a altura do alvo em metros com base no ID do AprilTag.
@@ -13,7 +15,7 @@ public class IdTargetHeight {
    * @param tagId o ID da AprilTag
    * @return a altura do alvo em metros
    */
-  public IdTargetHeight(int tagId) {
+  public double getTagHeight(int tagId) {
     switch (tagId) {
       case 1, 2, 5, 6, 9, 10 -> targetHeight = Units.inchesToMeters(53.38);
       case 3, 4, 7, 8 -> targetHeight = Units.inchesToMeters(57.13);
@@ -23,9 +25,17 @@ public class IdTargetHeight {
         targetHeight = 0.0;
       }
     }
+    return targetHeight;
   }
 
-  public double getTagMeters() {
-    return targetHeight;
+  /**
+   * Retorna a altura da câmera em metros pelo nome.
+   * @param camera nome da câmera usada
+   * @return altura câmera em metros
+   */
+  public double getCameraHeight(String camera){
+    if(camera.equals(VisionConstants.CAMERA_A_NAME)){
+      return VisionConstants.CAMERA_A_HEIGHT;
+    } return 0.0;
   }
 }
