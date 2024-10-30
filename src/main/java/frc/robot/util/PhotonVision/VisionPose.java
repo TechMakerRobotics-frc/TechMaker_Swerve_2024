1f0709c42dbbda83f07df2795eecf57510dba151
23Daniel3 <daneilcipriani321@gmail.com>
2024-10-30 20:08:25 -0300

    Resumidas importações

--- src/main/java/frc/robot/util/PhotonVision/VisionPose.java
+++ src/main/java/frc/robot/util/PhotonVision/VisionPose.java
@@ -2,18 +2,12 @@ package frc.robot.util.PhotonVision;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drive.Drive;
 
-import org.photonvision.EstimatedRobotPose;
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.*;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 import java.util.Optional;
