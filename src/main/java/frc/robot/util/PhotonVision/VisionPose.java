74a6d674c69e527d66746a9247cfe54a62c64e6e
23Daniel3 <daneilcipriani321@gmail.com>
2024-11-02 16:36:49 -0300

    Adicionada Pose da Limelight

--- src/main/java/frc/robot/util/PhotonVision/VisionPose.java
+++ src/main/java/frc/robot/util/PhotonVision/VisionPose.java
@@ -13,32 +13,42 @@ public class VisionPose extends SubsystemBase {
 
   private static final PhotonCamera FLcam = VisionTagsFLCam.getCamera();
   private static final PhotonCamera FRcam = VisionTagsFRCam.getCamera();
+  private static final PhotonCamera limelight = VisionTagsLimelight.getCamera();
 
   private AprilTagFieldLayout aprilTagFieldLayout =
       AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
 
   // Cam mounted facing forward, half a meter forward of center, half a meter up from center.
   private Transform3d robotToFLCam =
-      new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+      new Transform3d(new Translation3d(0.5, -0.25, 0.25), new Rotation3d(0, 0, 0));
 
   private PhotonPoseEstimator photonPoseEstimatorFLCam =
       new PhotonPoseEstimator(
           aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, FLcam, robotToFLCam);
 
   private Transform3d robotToFRCam =
-      new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+      new Transform3d(new Translation3d(0.5, 0.25, 0.25), new Rotation3d(0, 0, 0));
 
   private PhotonPoseEstimator photonPoseEstimatorFRCam =
       new PhotonPoseEstimator(
           aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, FRcam, robotToFRCam);
 
+  private Transform3d robotToLimelight =
+      new Transform3d(new Translation3d(0.2, 0.0, 0.30), new Rotation3d(0, 0, 0));
+
+  private PhotonPoseEstimator photonPoseEstimatorLimelight =
+      new PhotonPoseEstimator(
+          aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, limelight, robotToLimelight);
+
   private Drive drive;
 
   private Pose2d pose2dFLCam;
   private Pose2d pose2dFRCam;
+  private Pose2d pose2dLimelight;
 
   private Field2d fieldFLCam = new Field2d();
   private Field2d fieldFRCam = new Field2d();
+  private Field2d fieldLimelight = new Field2d();
 
   public VisionPose(Drive drive) {
     this.drive = drive;
@@ -48,8 +58,12 @@ public class VisionPose extends SubsystemBase {
   public void periodic() {
     Optional<EstimatedRobotPose> estimatedPoseOptFLCam =
         getEstimatedGlobalPoseFLCam(drive.getPose());
+
     Optional<EstimatedRobotPose> estimatedPoseOptFRCam =
         getEstimatedGlobalPoseFRCam(drive.getPose());
+
+    Optional<EstimatedRobotPose> estimatedPoseOptLimelight =
+        getEstimatedGlobalPoseLimelight(drive.getPose());
 
     if (estimatedPoseOptFLCam.isPresent()) {
       pose2dFLCam = estimatedPoseOptFLCam.get().estimatedPose.toPose2d();
@@ -64,6 +78,13 @@ public class VisionPose extends SubsystemBase {
 
       SmartDashboard.putData("Field to cam FR", fieldFRCam);
     }
+
+    if (estimatedPoseOptLimelight.isPresent()) {
+      pose2dLimelight = estimatedPoseOptLimelight.get().estimatedPose.toPose2d();
+      fieldLimelight.setRobotPose(pose2dLimelight);
+
+      SmartDashboard.putData("Field to cam Limelight", fieldLimelight);
+    }
   }
 
   public Optional<EstimatedRobotPose> getEstimatedGlobalPoseFLCam(Pose2d prevEstimatedRobotPose) {
@@ -74,5 +95,10 @@ public class VisionPose extends SubsystemBase {
   public Optional<EstimatedRobotPose> getEstimatedGlobalPoseFRCam(Pose2d prevEstimatedRobotPose) {
     photonPoseEstimatorFRCam.setReferencePose(prevEstimatedRobotPose);
     return photonPoseEstimatorFRCam.update();
+  }
+
+  public Optional<EstimatedRobotPose> getEstimatedGlobalPoseLimelight(Pose2d prevEstimatedRobotPose) {
+    photonPoseEstimatorLimelight.setReferencePose(prevEstimatedRobotPose);
+    return photonPoseEstimatorLimelight.update();
   }
 }
