e723a1d74bc3da1226dd2ff6ba251bbed8b1c1ea
23Daniel3 <daneilcipriani321@gmail.com>
2024-12-09 20:33:35 -0300

    TrajectoryCommand Funcional

--- src/main/java/frc/robot/commands/drive/TrajectoryCommand.java
+++ src/main/java/frc/robot/commands/drive/TrajectoryCommand.java
@@ -1,77 +1,38 @@
 package frc.robot.commands.drive;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
-import com.pathplanner.lib.path.PathPlannerPath;
-import edu.wpi.first.math.Pair;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.drive.Drive;
-import frc.robot.util.LocalADStarAK;
-import java.util.Arrays;
-import java.util.List;
 
 public class TrajectoryCommand extends Command {
-  private final LocalADStarAK pathfinder = new LocalADStarAK();
-  private final Translation2d startPose;
-  private final Translation2d endPose;
-  private PathPlannerPath currentPath;
-  private Translation2d goalTranslation = new Translation2d(5, 5);
+  private final Pose2d goalPose;
+  private final double timeOut;
   private PathConstraints constraints = new PathConstraints(14.5, 5, 6, 6);
-  private Rotation2d rotationGoalEndState = new Rotation2d(0, 0);
-  private GoalEndState goalEndState = new GoalEndState(0, rotationGoalEndState);
   private Command pathfollower;
+  private Timer time = new Timer();
 
-  // Lista de obstáculos (exemplo)
-  private final List<Translation2d> obstacles =
-      Arrays.asList(new Translation2d(2, 2), new Translation2d(5, 5), new Translation2d(6, 1));
-
-  public TrajectoryCommand(Drive drive) {
-    hasRequirement(drive);
-    this.startPose = drive.getPose().getTranslation();
-    this.endPose = goalTranslation;
+  public TrajectoryCommand(Pose2d goalPose, double timeOut) {
+    this.timeOut = timeOut;
+    this.goalPose = goalPose;
   }
 
   @Override
   public void initialize() {
-    pathfinder.setStartPosition(startPose);
-    pathfinder.setGoalPosition(endPose);
-
-    for (Translation2d obstacle : obstacles) {
-      pathfinder.setDynamicObstacles(List.of(new Pair<>(obstacle, obstacle)), startPose);
-    }
-
-    currentPath = pathfinder.getCurrentPath(constraints, goalEndState);
-    SmartDashboard.putBoolean("Current Path", currentPath != null);
-    if (currentPath != null) {
-      pathfollower = AutoBuilder.followPath(currentPath);
-      SmartDashboard.putBoolean("pathfollower", pathfollower != null);
-      pathfollower.initialize();
-    }
+    time.reset();
+    time.start();
+    pathfollower = AutoBuilder.pathfindToPose(goalPose, constraints);
+    pathfollower.initialize();
   }
 
   @Override
   public void execute() {
-    if (currentPath != null) {
-      pathfollower.execute();
-    }
-
-    SmartDashboard.putBoolean("Path Follower is not null?", pathfollower != null);
-    if (pathfollower != null) {
-      SmartDashboard.putBoolean("Is finished path?", pathfollower.isFinished());
-    }
+    pathfollower.execute();
   }
 
   @Override
   public boolean isFinished() {
-    return pathfollower == null;
-  }
-
-  @Override
-  public void end(boolean interrupted) {
-    // drive.stop();
+    return pathfollower == null || time.hasElapsed(timeOut);
   }
 }
